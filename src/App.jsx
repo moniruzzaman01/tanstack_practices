686cac69df7cd82ebf3c45d54f968b6fe2047bf2
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2023-12-05 00:51:21 +0600

    mutation practised

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import Mutation from "./components/Mutation";
+import { useQuery } from "@tanstack/react-query";
+
 function App() {
-  return <div>Hello</div>;
+  const loadPosts = async () => {
+    return fetch("http://localhost:5001/allPosts")
+      .then((res) => res.json())
+      .then((data) => data);
+  };
+
+  const { data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => loadPosts(),
+  });
+  console.log(data);
+
+  return (
+    <div>
+      <Mutation posts={data} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Mutation.jsx
@@ -0,0 +1,48 @@
+import { useMutation } from "@tanstack/react-query";
+
+function Mutation({ posts }) {
+  const insertPost = async (data) => {
+    fetch("http://localhost:5001/dummy", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => data);
+  };
+
+  const mutation = useMutation({
+    mutationFn: (data) => insertPost(data),
+  });
+
+  mutation.isSuccess ? console.log(`success`) : "";
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>slug</td>
+            <td>action</td>
+          </tr>
+        </thead>
+        {posts?.map((post, key) => (
+          <tbody key={key}>
+            <tr>
+              <td>{post._id}</td>
+              <td>{post.slug}</td>
+              <td>
+                <button onClick={() => mutation.mutate(post)}>click</button>
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </div>
+  );
+}
+
+export default Mutation;
